877863b39cf20e9d85086b4dbf32902e1ecbb0bb
Alen Rubilar <lclc.alen@gmail.com>
2022-01-14 01:38:44 -0300

    Lorem

--- setup.py
+++ setup.py
@@ -4,5 +4,7 @@ setuptools.setup(
     name="bund",
     version="0.0.0",
     packages=["bund"],
+    package_data={"": ["importer.template", "package.template"]},
+    include_package_data=True,
     python_requires=">=3.10",
 )
