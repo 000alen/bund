1143124f26f5686c863a650bc43ab9219ea15e54
Alen Rubilar <lclc.alen@gmail.com>
2022-01-14 01:11:31 -0300

    Lorem

--- bund/__init__.py
+++ bund/__init__.py
@@ -1,15 +1,19 @@
-__version__ = "0.0.0"
-
-
+from io import StringIO
 import os
 import string
 import json
-
-from bund.typings import CFG, File
+from typing import NamedTuple, Optional
 
 
 IMPORTER_FILE = "importer.template"
 PACKAGE_FILE = "package.template"
+
+
+class File(NamedTuple):
+    path: str
+    is_package: bool
+    code: str
+    timestamp: int
 
 
 def is_module(module: bool) -> bool:
@@ -21,29 +25,24 @@ def is_package(package: str) -> bool:
     return os.path.isdir(package) and os.path.isfile(init_file)
 
 
-def output(cfg: CFG, what: str):
-    cfg.outfile.write(what)
-
-
-def importer() -> str:
+def get_importer() -> str:
     importer_path = os.path.join(os.path.dirname(__file__), IMPORTER_FILE)
     with open(importer_path) as importer_object:
         importer = importer_object.read()
     return importer
 
 
-def template() -> string.Template:
+def get_template() -> string.Template:
     template_path = os.path.join(os.path.dirname(__file__), PACKAGE_FILE)
     with open(template_path) as template_object:
         template = string.Template(template_object.read())
     return template
 
 
-def process_file(base_dir: str, package_path: str) -> File:
+def collect_file(base_dir: str, package_path: str) -> File:
     path = os.path.splitext(package_path)[0].replace(os.path.sep, ".")
 
     full_path = os.path.join(base_dir, package_path)
-    print(full_path)
     with open(full_path, "r") as f:
         code = f.read()
 
@@ -55,35 +54,35 @@ def process_file(base_dir: str, package_path: str) -> File:
     return File(path, is_package, code, timestamp)
 
 
-def process_directory(base_dir: str, package_path: str) -> list[File]:
+def collect_directory(base_dir: str, package_path: str) -> list[File]:
     files = []
     contents = os.listdir(os.path.join(base_dir, package_path))
     for content in contents:
         next_path = os.path.join(package_path, content)
         path = os.path.join(base_dir, next_path)
         if is_module(path):
-            files.append(process_file(base_dir, next_path))
+            files.append(collect_file(base_dir, next_path))
         elif is_package(path):
-            files.extend(process_directory(base_dir, next_path))
+            files.extend(collect_directory(base_dir, next_path))
     return files
 
 
-def process_files(cfg: CFG):
-    package_template = template()
+def bundle(packages: list[str], default_package: str, outfile: StringIO, set_hook: Optional[bool] = None):
+    package_template = get_template()
 
     files = []
 
-    for package_path in cfg.packages:
+    for package_path in packages:
         base_dir, module_name = os.path.split(package_path)
-        files.extend(process_directory(base_dir, module_name))
+        files.extend(collect_directory(base_dir, module_name))
 
     packages = {path: data for path, *data in files}
 
-    output(cfg, package_template.substitute(
+    outfile.write(package_template.substitute(
         PACKAGES=json.dumps(packages).replace(
             "true", "True").replace("false", "False"),
-        FORCE_EXC_HOOK=str(cfg.set_hook),
-        DEFAULT_PACKAGE=json.dumps(cfg.default_package),
-        IMPORTER=json.dumps(importer())
+        FORCE_EXC_HOOK=str(set_hook),
+        DEFAULT_PACKAGE=json.dumps(default_package),
+        IMPORTER=json.dumps(get_importer())
     ))
-    cfg.outfile.close()
+    outfile.close()
--- bund/__main__.py
+++ bund/__main__.py
@@ -1,48 +1,51 @@
-from bund import process_files, __version__, is_package
+from io import StringIO
+from typing import Optional, TypedDict
+from bund import bundle
 import argparse
-import os
 import sys
 
-from bund.typings import CFG
+
+class Args(TypedDict):
+    packages: list[str]
+    default_package: str
+    outfile: StringIO
+    set_hook: Optional[bool]
 
 
-def parse_args() -> CFG:
-    parser = argparse.ArgumentParser()
-    parser.add_argument("packages", nargs="+")
-    parser.add_argument("--version", action="version", version=__version__)
-    parser.add_argument("-o", "--outfile", nargs="?",
-                        type=argparse.FileType("w"),
-                        default=sys.stdout)
-    parser.add_argument("--set-except", default=None, dest="set_hook",
-                        action="store_true")
-    parser.add_argument("--no-except", default=None, dest="set_hook",
-                        action="store_false",)
-    parser.add_argument("-d", "--default-pkg", default=None,
-                        dest="default_package")
-    cfg = parser.parse_args()
-    if cfg.default_package is None:
-        def_file = cfg.packages[0] if len(cfg.packages) == 1 else ""
-        cfg.default_package = def_file
-    return cfg
+parser = argparse.ArgumentParser()
+parser.add_argument(
+    "packages",
+    nargs="+"
+)
+parser.add_argument(
+    "-o",
+    "--outfile",
+    nargs="?",
+    type=argparse.FileType("w"),
+    default=sys.stdout
+)
+parser.add_argument(
+    "--set-except",
+    default=None,
+    dest="set_hook",
+    action="store_true"
+)
+parser.add_argument(
+    "--no-except",
+    default=None,
+    dest="set_hook",
+    action="store_false"
+)
+parser.add_argument(
+    "-d",
+    "--default-pkg",
+    default=None,
+    dest="default_package"
+)
+
+args: Args = parser.parse_args()
+if args.default_package is None:
+    args.default_package = args.packages[0]
 
 
-def validate_args(cfg):
-    missing = False
-    for package in cfg.packages:
-        if not is_package(package):
-            sys.stderr.write("ERROR: %s is not a python package" % package)
-            missing = True
-    if missing:
-        sys.exit(1)
-
-    if cfg.default_package:
-        if cfg.default_package not in cfg.packages:
-            sys.stderr.write("ERROR: %s is not a valid default package" %
-                             cfg.default_pkg)
-            sys.exit(2)
-        cfg.default_package = os.path.split(cfg.default_package)[1]
-
-
-cfg = parse_args()
-validate_args(cfg)
-process_files(cfg)
+bundle(args.packages, args.default_package, args.outfile, args.set_hook)
